b24fef82924d97148f5e08ea8253a8aa0d8c4920
adimar <adimaraisha@gmail.com>
2023-05-02 14:41:58 +0800

    changed app logo

--- components/main/MainHeader.js
+++ components/main/MainHeader.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import Alert from "../elements/Alert";
 import NotificationMain from "../notification/NotificationMain";
@@ -17,14 +18,24 @@ export default function MainHeader({ className, logoClass }) {
         <div className="flex gap-2 items-center">
           <MainBurgerMenu />
           <Link href="/feed">
-            <h1
-              className={
-                "font-verdana   flex items-center  font-bold text-4xl text-primary-light dark:text-pink-300 cursor-pointer " +
-                logoClass
-              }
-            >
-              kenal
-            </h1>
+            <div className="flex">
+              <div className="min-w-[30px] relative">
+                <Image
+                  src={"/logo.png"}
+                  layout="fill"
+                  objectFit="cover"
+                  alt="logo"
+                />
+              </div>
+              <h1
+                className={
+                  "font-verdana m-0 p-0  flex items-center  font-bold text-4xl text-primary-light dark:text-pink-300 cursor-pointer " +
+                  logoClass
+                }
+              >
+                enal
+              </h1>
+            </div>
           </Link>
         </div>
         <div className="flex items-center gap-3">
